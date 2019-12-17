bb1efae06d4507a2145e541bfd4882f370984e4c
Aden Albert <ghost@adenalbert.com>
2019-12-17 17:00:34 -0500

    Built GER dictionary and lister function

--- automator.py
+++ automator.py
@@ -6,6 +6,26 @@ import time
 # GLOBAL ----------------------------------------------------------------------
 FILE_LOCATION = '/mnt/c/Users/talbert/Downloads/linux/'
 
+STARTER_COLUMN = 10
+
+GER_COLUMN = {
+'FYW': STARTER_COLUMN, 
+'WR': STARTER_COLUMN + 3, 
+'FYS': STARTER_COLUMN + 3, 
+'NWL': STARTER_COLUMN + 6, 
+'NW': STARTER_COLUMN + 9, 
+'HB': STARTER_COLUMN + 12, 
+'TA': STARTER_COLUMN + 18, 
+'HA': STARTER_COLUMN + 21, 
+'VP': STARTER_COLUMN + 24, 
+'MR': STARTER_COLUMN + 27, 
+'FL': STARTER_COLUMN + 30, 
+'UQ': STARTER_COLUMN + 33, 
+'MB': STARTER_COLUMN + 36, 
+'NE': STARTER_COLUMN + 39, 
+'WC': STARTER_COLUMN + 42
+}
+
 
 # FUNCTION --------------------------------------------------------------------
 def source_sheet():
@@ -36,15 +56,37 @@ def validate_grade(grade):
 # END validator()
 
 
+def validate_credit(credit_type):
+    '''
+        Process different credit types.
+    '''
+    pass
+# END validate_credit()
+
+
 def format_term(course_term):
     '''
         Returns the term a course was taken in the format 
         FA ##/SP ##/SU ##.
     '''
-    terms = {'D1': 'FA', 'D2': 'SP', 'D4': 'SU', '  ': 'EQUIV'}
-    term = terms[course_term[4:6]] + ' ' + course_term[2:4]
-    return term
+    terms = {'D1': 'FA', 'D2': 'SP', 'D4': 'SU'}
+    if course_term != None:
+        term = terms[course_term[4:6]] + ' ' + course_term[2:4]
+        return term
+    
+    return None
 # END format_term()
+
+
+def ger(student, ger, course, term, grade):
+    '''
+        Scan a GER type, and apply it to the correct column.
+    '''
+    
+    student[GER_COLUMN[ger]].value = course
+    student[GER_COLUMN[ger] + 1].value = term
+    student[GER_COLUMN[ger] + 2].value = grade
+# END ger()
 
 
 # MAIN ------------------------------------------------------------------------
@@ -62,79 +104,20 @@ for student in target_sheet.iter_rows(min_row=2):
     # Iterate through all courses in Automator Course List
     for course in source_sheet.iter_rows(min_row=2):
         
+        # Validate grade (i.e., don't waste time on fails or PNP)
         course_grade = course[5].value
         if validate_grade(course_grade):
 
-            course_id = course[0].value
-            course_code = course[2].value
-            course_term = course[1].value
-            course_ger = course[6].value
+            # Make values human-parsable
+            #course_id = course[0].value
+            #course_code = course[2].value
+            #course_term = format_term(course[1].value)
+            #course_ger = course[6].value
 
-            if target_id == course_id:
-                if course_ger == 'FYW':
-                    student[10].value = course_code
-                    student[11].value = course_term
-                    student[12].value = course_grade
-
-                if course_ger == 'WR':
-                    student[13].value = course_code
-                    student[14].value = course_term
-                    student[15].value = course_grade
-
-                if course_ger == 'NWL':
-                    student[16].value = course_code
-                    student[17].value = course_term
-                    student[18].value = course_grade
-
-                if course_ger == 'HB':
-                    student[22].value = course_code
-                    student[23].value = course_term
-                    student[24].value = course_grade
-
-                if course_ger == 'TA':
-                    student[28].value = course_code
-                    student[29].value = course_term
-                    student[30].value = course_grade
-
-                if course_ger == 'HA':
-                    student[31].value = course_code
-                    student[32].value = course_term
-                    student[33].value = course_grade
-
-                if course_ger == 'VP':
-                    student[34].value = course_code
-                    student[35].value = course_term
-                    student[36].value = course_grade
-
-                if course_ger == 'MR':
-                    student[37].value = course_code
-                    student[38].value = course_term
-                    student[39].value = course_grade
-
-                if course_ger == 'FL':
-                    student[40].value = course_code
-                    student[41].value = course_term
-                    student[42].value = course_grade
-
-                if course_ger == 'UQ':
-                    student[43].value = course_code
-                    student[44].value = course_term
-                    student[45].value = course_grade
-
-                if course_ger == 'MB':
-                    student[46].value = course_code
-                    student[47].value = course_term
-                    student[48].value = course_grade
-
-                if course_ger == 'NE':
-                    student[49].value = course_code
-                    student[50].value = course_term
-                    student[51].value = course_grade
-
-                if course_ger == 'WC':
-                    student[52].value = course_code
-                    student[53].value = course_term
-                    student[54].value = course_grade
+            # If student and course match, and the course fulfills a GER,
+            # pass the information along to the GER column sorter.
+            if target_id == course[0].value and course[6].value != None:
+                ger(student, course[6].value, course[2].value, format_term(course[1].value), course_grade)
 
 # Print test results
 t1 = time.process_time()
