e24ac1572a706232761a6d1fafaa791461785654
Aden Albert <ghost@adenalbert.com>
2019-12-17 09:34:00 -0500

    Adjusted position of grade validation in loop

--- automator.py
+++ automator.py
@@ -55,83 +55,87 @@ target_sheet = target_book['Automator Student List']
 print("Test begins: ")
 t0 = time.process_time()
 
+# Iterate through all students in Automator Student List
 for student in target_sheet.iter_rows(min_row=2):
     target_id = student[0].value
-    nwl_count = 0
 
+    # Iterate through all courses in Automator Course List
     for course in source_sheet.iter_rows(min_row=2):
-        course_id = course[0].value
-        course_code = course[2].value
-        course_term = course[1].value
+        
         course_grade = course[5].value
-        course_ger = course[6].value
+        if validator(course_grade):
 
-        if target_id == course_id and validator(course_grade):
-            if course_ger == 'FYW':
-                student[10].value = course_code
-                student[11].value = course_term
-                student[12].value = course_grade
+            course_id = course[0].value
+            course_code = course[2].value
+            course_term = course[1].value
+            course_ger = course[6].value
 
-            if course_ger == 'WR':
-                student[13].value = course_code
-                student[14].value = course_term
-                student[15].value = course_grade
+            if target_id == course_id:
+                if course_ger == 'FYW':
+                    student[10].value = course_code
+                    student[11].value = course_term
+                    student[12].value = course_grade
 
-            if course_ger == 'NWL':
-                student[16].value = course_code
-                student[17].value = course_term
-                student[18].value = course_grade
-                nwl_count = 1
+                if course_ger == 'WR':
+                    student[13].value = course_code
+                    student[14].value = course_term
+                    student[15].value = course_grade
 
-            if course_ger == 'HB':
-                student[22].value = course_code
-                student[23].value = course_term
-                student[24].value = course_grade
+                if course_ger == 'NWL':
+                    student[16].value = course_code
+                    student[17].value = course_term
+                    student[18].value = course_grade
+                    nwl_count = 1
 
-            if course_ger == 'TA':
-                student[28].value = course_code
-                student[29].value = course_term
-                student[30].value = course_grade
+                if course_ger == 'HB':
+                    student[22].value = course_code
+                    student[23].value = course_term
+                    student[24].value = course_grade
 
-            if course_ger == 'HA':
-                student[31].value = course_code
-                student[32].value = course_term
-                student[33].value = course_grade
+                if course_ger == 'TA':
+                    student[28].value = course_code
+                    student[29].value = course_term
+                    student[30].value = course_grade
 
-            if course_ger == 'VP':
-                student[34].value = course_code
-                student[35].value = course_term
-                student[36].value = course_grade
+                if course_ger == 'HA':
+                    student[31].value = course_code
+                    student[32].value = course_term
+                    student[33].value = course_grade
 
-            if course_ger == 'MR':
-                student[37].value = course_code
-                student[38].value = course_term
-                student[39].value = course_grade
+                if course_ger == 'VP':
+                    student[34].value = course_code
+                    student[35].value = course_term
+                    student[36].value = course_grade
 
-            if course_ger == 'FL':
-                student[40].value = course_code
-                student[41].value = course_term
-                student[42].value = course_grade
+                if course_ger == 'MR':
+                    student[37].value = course_code
+                    student[38].value = course_term
+                    student[39].value = course_grade
 
-            if course_ger == 'UQ':
-                student[43].value = course_code
-                student[44].value = course_term
-                student[45].value = course_grade
+                if course_ger == 'FL':
+                    student[40].value = course_code
+                    student[41].value = course_term
+                    student[42].value = course_grade
 
-            if course_ger == 'MB':
-                student[46].value = course_code
-                student[47].value = course_term
-                student[48].value = course_grade
+                if course_ger == 'UQ':
+                    student[43].value = course_code
+                    student[44].value = course_term
+                    student[45].value = course_grade
 
-            if course_ger == 'NE':
-                student[49].value = course_code
-                student[50].value = course_term
-                student[51].value = course_grade
+                if course_ger == 'MB':
+                    student[46].value = course_code
+                    student[47].value = course_term
+                    student[48].value = course_grade
 
-            if course_ger == 'WC':
-                student[52].value = course_code
-                student[53].value = course_term
-                student[54].value = course_grade
+                if course_ger == 'NE':
+                    student[49].value = course_code
+                    student[50].value = course_term
+                    student[51].value = course_grade
+
+                if course_ger == 'WC':
+                    student[52].value = course_code
+                    student[53].value = course_term
+                    student[54].value = course_grade
 
 # Print test results
 t1 = time.process_time()
