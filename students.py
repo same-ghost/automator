e35406f088090faa6c04a0c08729ef3f901d4d27
Aden Albert <ghost@adenalbert.com>
2019-12-20 15:13:42 -0500

    Began building Student() class

--- students.py
+++ students.py
@@ -3,6 +3,14 @@ class Degree:
 
     def __init__(self, type):
         self.type = type
+        self.gers = ['FYW', 'WR', 'NWL', 'NWL', 'HB1', 'HB2', 'TA', 'HA', 'VP',
+        'MR', 'FL', 'UQ', 'MB', 'NE', 'WC']
+
+        if self.type == 'bm':
+            self.gers.pop(3)
+            self.gers.pop(8)
+        elif self.type == 'ba':
+            self.gers[3] = 'NW'
     # END Degree.__init__()
 
 
@@ -12,9 +20,25 @@ class Degree:
     # END Degree.__str__()
 
 
-    def audit(self):
-        pass
-    # END Degree.audit()
-
-
 # END Degree()
+
+
+class Student:
+
+
+    def __init__(self, id='0', name='', degree='ba'):
+        self.id = id
+        self.name = name
+        self.degree = Degree(degree)
+        self.completed = []
+    # END Student.__init__()
+
+
+    def __str__(self):
+        return f"{self.id} {self.name} {self.degree}"
+    # END Student.__str__()
+
+
+    def add_ger(self, course, term, grade, ger):
+        pass
+# END Student()
